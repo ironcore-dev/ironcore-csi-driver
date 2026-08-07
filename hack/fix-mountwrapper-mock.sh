5b8a79ba90b61e3eaea2f1cadfd72c04f52d1abc
Rohit Kumar <rohit.1si09ee045@gmail.com>
2026-08-07 13:40:40 +0530

    update script as per reivew discussion
    Signed-off-by: Rohit Kumar <rohit.1si09ee045@gmail.com>

--- hack/fix-mountwrapper-mock.sh
+++ hack/fix-mountwrapper-mock.sh
@@ -6,12 +6,8 @@ FILE="pkg/utils/mount/mock_mountutils_unix.go"
 # Fixes "MountWrapper Type cannot implement 'MountWrapper' as it has a non-exported method and is defined in a different package"
 # See https://github.com/kubernetes/mount-utils/commit/a20fcfb15a701977d086330b47b7efad51eb608e for context.
 
-if [[ "$(uname)" == "Darwin" ]]; then
-  # macOS sed requires an empty string ('') right after -i.
-  sed -i '' '/type MockMountWrapper struct {/a\
+# Standard POSIX stream editing (works perfectly on ALL sed versions)
+sed '/type MockMountWrapper struct {/a \
 	mount.Interface
-' "$FILE"
-else
-  # Linux sed works with the -i flag as is.
-  sed -i '/type MockMountWrapper struct {/a \\tmount.Interface' "$FILE"
-fi
+' "$FILE" > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"
+
